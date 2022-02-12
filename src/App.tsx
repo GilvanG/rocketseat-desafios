1da25f2e2daa82728a825d9a11a0be234bc0df5e
gilvan <gilvango.j@gmail.com>
2022-02-12 09:25:54 -0300

    Resposta do Desafio 2 do Capítulo 1

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+import { Content } from "./components/Content";
+import { SideBar } from "./components/SideBar";
+
+import "./styles/global.scss";
+
+export function App() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <SideBar
+        selectedGenreId={selectedGenreId}
+        handleClickButton={handleClickButton}
+      />
+
+      <Content selectedGenreId={selectedGenreId} />
+    </div>
+  );
+}
